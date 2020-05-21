76c4fdbf1142521b7ee41ccb5d4245891443d5bd
Vladislav Kibenko <wolfram.deus@gmail.com>
2020-05-21 19:09:16 +0500

    fix(query): fixed query creation

--- src/utils.ts
+++ src/utils.ts
@@ -1,23 +1,5 @@
-/**
- * Converts value to VKontakte API query compatible format
- * @param value
- * @returns {string}
- */
 import {toSnakeCase} from './VKAPI';
-
-export function stringifyValue(value: any): string {
-  if (Array.isArray(value)) {
-    return value.map(stringifyValue).join(',');
-  }
-
-  if (typeof value === 'object') {
-    return Object.keys(value)
-      .map(key => `${key}=${stringifyValue(value[key])}`)
-      .join('&');
-  }
-
-  return escape(String(value));
-}
+import {stringify} from 'querystring';
 
 /**
  * Creates query converting camel case field names to snake case
@@ -25,10 +7,15 @@ export function stringifyValue(value: any): string {
  * @returns {string}
  */
 export function formatQuery(query: Record<string, any>): string {
-  return Object.keys(query).reduce<string>((acc, key, idx) => {
-    if (idx !== 0) {
-      acc += '&';
-    }
-    return `${acc}${toSnakeCase(key)}=${stringifyValue(query[key])}`;
-  }, '');
+  const preparedObject = Object
+    .keys(query)
+    .reduce<Record<string, any>>((acc, key) => {
+      acc[toSnakeCase(key)] = Array.isArray(query[key])
+        ? query[key].join(',')
+        : query[key];
+
+      return acc;
+    }, {});
+
+  return stringify(preparedObject);
 }
