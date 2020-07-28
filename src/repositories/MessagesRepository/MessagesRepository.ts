ccdff58d51d347f3d36be4cb6c9d48b4f7a0b14e
Vladislav Kibenko <wolfram.deus@gmail.com>
2020-07-28 19:49:40 +0500

    fix(messages.send): format userIds if passed

--- src/repositories/MessagesRepository/MessagesRepository.ts
+++ src/repositories/MessagesRepository/MessagesRepository.ts
@@ -1,6 +1,7 @@
 import {Repository} from '../Repository';
 import {SendRequest} from '../../types';
 import {SendParams, SendResult} from './types';
+import {formatOptionalArray} from '../../utils';
 
 export class MessagesRepository extends Repository {
   constructor(sendRequest: SendRequest) {
@@ -9,11 +10,20 @@ export class MessagesRepository extends Repository {
 
   public send = this.r<SendParams, SendResult>(
     'send',
-    ({randomId, ...rest}) => ({
-      ...rest,
-      randomId: typeof randomId === 'undefined'
+    ({randomId: _randomId, ...rest}) => {
+      const randomId = typeof _randomId === 'undefined'
         ? Math.floor(Math.random() * 10000000)
-        : randomId,
-    }),
+        : _randomId;
+
+      if ('userIds' in rest) {
+        return {
+          ...rest,
+          userIds: formatOptionalArray(rest.userIds),
+          randomId,
+        };
+      }
+
+      return {...rest, randomId};
+    },
   );
 }
