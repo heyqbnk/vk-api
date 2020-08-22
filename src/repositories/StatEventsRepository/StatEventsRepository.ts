0cf980140999377a5245f1ee8dac146f36485b68
Vladislav Kibenko <wolfram.deus@gmail.com>
2020-08-22 17:15:41 +0500

    feat(statevents): add addMiniApps method

--- src/repositories/StatEventsRepository/StatEventsRepository.ts
+++ src/repositories/StatEventsRepository/StatEventsRepository.ts
@@ -1,6 +1,10 @@
 import {Repository} from '../Repository';
 import {SendRequest} from '../../types';
-import {AddMiniAppsCustomParams, AddMiniAppsCustomResult} from './types';
+import {
+  AddMiniAppsCustomParams,
+  AddMiniAppsCustomResult,
+  AddMiniAppsParams,
+} from './types';
 
 export class StatEventsRepository extends Repository {
   constructor(sendRequest: SendRequest) {
@@ -8,9 +12,16 @@ export class StatEventsRepository extends Repository {
   }
 
   /**
-   * @see addMiniAppsCustom
-   * @type {RepositoryMethod<GetCommentsParams, GetCommentsResult>}
+   * @see https://vk.com/dev/statEvents.addMiniAppsCustom
+   * @type {RepositoryMethod<AddMiniAppsCustomParams, AddMiniAppsCustomResult>}
    */
   addMiniAppsCustom = this.r<AddMiniAppsCustomParams,
     AddMiniAppsCustomResult>('addMiniAppsCustom');
+
+  /**
+   * @see https://vk.com/dev/statEvents.addMiniApps
+   * @type {RepositoryMethod<AddMiniAppsParams, AddMiniAppsCustomResult>}
+   */
+  addMiniApps = this.r<AddMiniAppsParams,
+    AddMiniAppsCustomResult>('addMiniApps');
 }
--- src/repositories/StatEventsRepository/types.ts
+++ src/repositories/StatEventsRepository/types.ts
@@ -9,3 +9,11 @@ export interface AddMiniAppsCustomParams {
 }
 
 export type AddMiniAppsCustomResult = 1;
+
+/**
+ * @see https://vk.com/dev/statEvents.addMiniApps
+ */
+export interface AddMiniAppsParams extends AddMiniAppsCustomParams {
+}
+
+export type AddMiniAppsResult = AddMiniAppsCustomResult;
